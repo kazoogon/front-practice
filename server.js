6a0643e9fd0393231bccdc32f05bc06b84301d1f
kakaz <kakazoo209@gmail.com>
2018-09-10 11:52:15 +0200

    to solve error 'because its MIME type ('text/html') is not a supported stylesheet'

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+var mime = {
+    ".html": "text/html",
+    ".css":  "text/css"
+};
 
 fs.readFile('./index.html', function (err, html) {
 
     if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+    http.createServer(function(request, res) {  
+        res.writeHead(200, {"Content-Type": mime[path.extname(fullPath)] || "text/plain"});  
+        res.write(html);  
+        res.end();  
     }).listen(process.env.PORT || 5000)
 });
